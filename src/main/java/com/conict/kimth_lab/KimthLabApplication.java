8faceb3b14dca60dddf5c9676923b0c7464a7ce8
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-19 22:30:14 +0900

    setting: springboot default setting clear

---
+++ src/main/java/com/conict/kimth_lab/KimthLabApplication.java
@@ -0,0 +1,11 @@
+package com.conict.kimth_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimthLabApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(KimthLabApplication.class, args);
+  }
+}
---
+++ src/main/java/com/conict/kimth_lab/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.conict.kimth_lab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    Docket docket = new Docket(DocumentationType.OAS_30)
+      .useDefaultResponseMessages(false)
+      .select()
+      .apis(RequestHandlerSelectors.basePackage("com.conict.kimth_lab.controller"))
+      .paths(PathSelectors.any())
+      .build()
+      .apiInfo(apiInfo());
+    return docket;
+  }
+
+  private ApiInfo apiInfo() {
+    ApiInfo builder = new ApiInfoBuilder()
+      .title("kimth_lab API Docs")
+      .description("API Docs for kimth_lab")
+      .version("1.0.0")
+      .build();
+    return builder;
+  }
+}
