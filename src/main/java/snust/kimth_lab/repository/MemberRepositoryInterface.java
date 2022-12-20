601768e895fb89560daad0d4b65ba2ed9efb8d13
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-21 04:37:09 +0900

    add: add overide method in repo

--- src/main/java/snust/kimth_lab/repository/MemberRepositoryInterface.java
+++ src/main/java/snust/kimth_lab/repository/MemberRepositoryInterface.java
@@ -1,9 +1,7 @@
 package snust.kimth_lab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import snust.kimth_lab.entity.Member;
 
-@Repository
 public interface MemberRepositoryInterface extends JpaRepository<Member, Long> {
 }
