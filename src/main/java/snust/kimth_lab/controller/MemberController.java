732c72eab11c78d589a4f90c33621606e874f529
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-20 23:33:13 +0900

    enhancement: setup member controller methods, except for token manager entity(cause token entity doesn't be made yet.), close #2

--- src/main/java/snust/kimth_lab/controller/MemberController.java
+++ src/main/java/snust/kimth_lab/controller/MemberController.java
@@ -1,2 +1,24 @@
-package snust.kimth_lab.controller;public class MemberController {
+package snust.kimth_lab.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import snust.kimth_lab.dto.member.SignInDto;
+import snust.kimth_lab.entity.Member;
+
+@RequestMapping("/member")
+@RestController
+public class MemberController {
+  @PostMapping("/signup")
+  public ResponseEntity<Long> signup(@RequestBody Member member) {
+    return new ResponseEntity<>(1L, HttpStatus.CREATED);
+  }
+
+  @PostMapping("/signin")
+  public ResponseEntity<String> signin(@RequestBody SignInDto signInDto) {
+    return new ResponseEntity<>("token!", HttpStatus.OK);
+  }
 }
