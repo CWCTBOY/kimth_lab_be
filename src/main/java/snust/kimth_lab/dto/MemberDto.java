b3595d4a0a921bf2cc42dfa5352fe26bf74d0222
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-20 18:22:43 +0900

    enhancement: build member dto would be sended to client, close #1

--- src/main/java/snust/kimth_lab/dto/MemberDto.java
+++ src/main/java/snust/kimth_lab/dto/MemberDto.java
@@ -1,2 +1,25 @@
-package snust.kimth_lab.dto;public class MemberDto {
+package snust.kimth_lab.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import snust.kimth_lab.entity.Member;
+import snust.kimth_lab.entity.Project;
+
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Getter
+@Setter
+public class MemberDto extends Member {
+  @JsonIgnore
+  @OneToMany(mappedBy = "userId")
+  private List<Project> myProject;
+
+  @Builder
+  public MemberDto(List<Project> myProject) {
+    super();
+    this.myProject = myProject;
+  }
 }
