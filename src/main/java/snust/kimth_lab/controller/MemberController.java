97b81016507cc381c3a766f244104fa43c5d35bc
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-21 04:39:01 +0900

    enhancement: setup the frame of the member controller

--- src/main/java/snust/kimth_lab/controller/MemberController.java
+++ src/main/java/snust/kimth_lab/controller/MemberController.java
@@ -1,20 +1,27 @@
 package snust.kimth_lab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import snust.kimth_lab.dto.member.SignInDto;
 import snust.kimth_lab.entity.Member;
+import snust.kimth_lab.service.member.MemberServiceInterface;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/member")
 @RestController
 public class MemberController {
+  private final MemberServiceInterface memberService;
+
+  @Autowired
+  public MemberController(MemberServiceInterface memberService) {
+    this.memberService = memberService;
+  }
+
   @PostMapping("/signup")
   public ResponseEntity<Long> signup(@RequestBody Member member) {
-    return new ResponseEntity<>(1L, HttpStatus.CREATED);
+    return null;
   }
 
   @PostMapping("/signin")
