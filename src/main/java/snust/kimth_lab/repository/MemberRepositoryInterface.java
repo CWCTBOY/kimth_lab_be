12435270c191c0d28f34dd7ed1382e6e5ffd7dd7
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-21 20:25:06 +0900

    add: add jpa method 'findByEmail' in member repo interface

--- src/main/java/snust/kimth_lab/repository/MemberRepositoryInterface.java
+++ src/main/java/snust/kimth_lab/repository/MemberRepositoryInterface.java
@@ -3,5 +3,8 @@ package snust.kimth_lab.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import snust.kimth_lab.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepositoryInterface extends JpaRepository<Member, Long> {
+  Optional<Member> findByEmail(String email);
 }
