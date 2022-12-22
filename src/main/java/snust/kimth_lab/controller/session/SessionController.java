5e9f1ed13f281c96067f1c1dbf1394009f974cf7
CWCTBOY <qkrdlswo98@gmail.com>
2022-12-22 19:33:34 +0900

    enhancement: setup session controller

--- src/main/java/snust/kimth_lab/controller/session/SessionController.java
+++ src/main/java/snust/kimth_lab/controller/session/SessionController.java
@@ -1,19 +1,25 @@
-//package snust.kimth_lab.controller.session;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpSession;
-//
-//@RestController
-//public class SessionController {
-//  @GetMapping("/session-info")
-//  public ResponseEntity sessionInfo(HttpServletRequest req) {
-//    HttpSession session = req.getSession(false);
-//    if (session == null) {
-//      return ResponseEntity.status();
-//    }
-//  }
-//}
+package snust.kimth_lab.controller.session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import snust.kimth_lab.dto.response.session.SessionResDto;
+import snust.kimth_lab.util.session.SessionManagerInterface;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class SessionController {
+  SessionManagerInterface sessionManager;
+
+  @Autowired
+  public SessionController(SessionManagerInterface sessionManager) {
+    this.sessionManager = sessionManager;
+  }
+
+  @GetMapping("/session-info")
+  public ResponseEntity<SessionResDto> sessionInfo(HttpServletRequest request) {
+    return null;
+  }
+}
